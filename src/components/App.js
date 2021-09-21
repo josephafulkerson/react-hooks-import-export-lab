1c3cb303a2bbdecbd0ba51ae008fa798e1b44d0c
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-09-21 18:32:31 -0400

    all tests pass

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import Home from "./Home";
+import About from "./About";
+import NavBar from "./NavBar";
+import { image, username, city } from '../data/user'
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={username} city={city}/>
+      <About image={image}/>
     </div>
   );
 }
